ee08577a1afd5de15f66482c8d2ccfd430ef88e1
ayush <ayush.chaurasia68789@gmail.com>
2026-06-15 21:25:18 +0530

    feat : Deleting Product also Deletes it's reviews from DB by adding pre post middleware for findOneAndDelete

--- models/Product.js
+++ models/Product.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose') ; 
+const Reviews = require('./Review');
 
 const productSchema = new mongoose.Schema({
     name : {
@@ -25,6 +26,20 @@ const productSchema = new mongoose.Schema({
         }
     ]
 })
+
+// Middle wasre jo bts mongodb operations krwane pr use hota hai and iske andr pre and post middle ware hote hein 
+// which is basically used over the schema and before the model is js class  pre mtlb pehle post mtlb baad mein
+// to idhr mujhe product ke delete hone ke pehle uske reviews ko delete krna h so well use pre
+
+productSchema.post('findOneAndDelete' , async (prod)=> {
+    if(prod.reviews.length > 0)
+    {
+        await Reviews.deleteMany({
+            _id:{$in:prod.reviews}
+        }) ;
+    }
+})
+
 
 const Product = mongoose.model('Product', productSchema) ;
 
